4568a26499cda436fdae2b4fe9b0694a03bbedf7
BUKA <avanashvili5@yahoo.com>
2015-10-09 13:58:05 +0400

    implementing page controller item

---
+++ IOS.UIPageViewController/UIPageViewControllerItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UIKit;
+using CoreGraphics;
+
+namespace IOS.UIPageViewController
+{
+	public class UIPageViewControllerItem : UIViewController
+	{
+		#region Constructor
+
+		public UIPageViewControllerItem (int index)
+		{
+			this.Index = index;
+		}
+
+		#endregion
+
+		#region Properties
+
+		public int Index {
+			get;
+			set;
+		}
+
+		#endregion
+
+		#region Overrides
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			View.BackgroundColor = UIColor.LightGray;
+			InitUI ();
+		}
+
+		#endregion
+
+		#region Methods
+
+		private void InitUI ()
+		{
+			var padding = 100f;
+			UIView mainView = new UIView (new CGRect (padding / 2f, padding, View.Frame.Width - padding, View.Frame.Height - padding * 2));
+
+			var indexLabelHeight = 30f;
+			UILabel indexLabel = new UILabel (new CGRect (0, 
+				                     mainView.Frame.Height / 2 - indexLabelHeight / 2f,
+				                     mainView.Frame.Width,
+				                     indexLabelHeight));
+			indexLabel.Text = Index.ToString ();
+			indexLabel.TextColor = UIColor.Red;
+			indexLabel.Font = UIFont.SystemFontOfSize (22);
+			mainView.AddSubview (indexLabel);
+
+			View.AddSubview (mainView);
+		}
+
+		#endregion
+	}
+}
+
